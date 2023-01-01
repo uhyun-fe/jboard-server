b0f628fcd9a7b5f5704e08d849ce518ef61c8700
uhyun-fe <feyouhyun0957@gmail.com>
2023-01-01 10:41:11 +0900

    feat: add get list router

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express!!!');
+    res.send('Hello, This is jboard project!');
+});
+
+const boardList = [
+    {idx: 1, title: "아싸 첫글!", writer: {idx: 3, user_id: "kado", user_name: "카도쨩"}, views: 15},
+];
+
+app.get('/list', (req, res) => {
+    res.send(boardList);
 });
 
 app.listen(app.get('port'), () => {
